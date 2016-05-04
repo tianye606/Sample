81a16968736b00d4256a864250afbbc9538fef86
Odysseus Yang <yongyang@microsoft.com>
2016-05-04 16:38:07 -0700

    Enable WPP trace on user-mode application

--- DriverSample/WPP/WPPSample.cpp
+++ DriverSample/WPP/WPPSample.cpp
@@ -3,9 +3,22 @@
 
 #include "stdafx.h"
 #include "WPPTrace.h"
+#include "WPPSample.tmh"
 
-int _tmain(int argc, _TCHAR* argv[])
+int __cdecl _tmain(int argc, _TCHAR* argv[])
 {
+    //Need Modify the Visual Studio project to run the WPP preprocessor and build the solution.
+    //1         Right click the Applciation project in Solutions Explorer and click Properties.
+    //2         In the project property page, click Configuration Properties and click WPP Tracing.
+    //3         Under General, set the Run WPP option to Yes and Enable Minimal Rebuild to N0
+    //4         Under File Options set the Scan Configuration Data to .\trace.h(where you have declared the TraceEvent)
+
+    //Rember to clean build, or change in WPPTrace.tmh won't be updated even if WPPTrace.h is updated
+    WPP_INIT_TRACING(NULL);
+    TraceEntry();
+    TraceEvents(TRACE_LEVEL_INFORMATION, WPP_BIT_ALL_INFO, _TEXT("WPP Trace Events"));
+    TRACE_RETURN(0);
+    WPP_CLEANUP();
 	return 0;
 }
 
--- DriverSample/WPP/WPPTrace.h
+++ DriverSample/WPP/WPPTrace.h
@@ -3,10 +3,10 @@
 #define WPP_CONTROL_GUIDS                                              \
     WPP_DEFINE_CONTROL_GUID(                                           \
         myDriverTraceGuid, (84bdb2e9,829e,41b3,b891,02f454bc2bd7), \
-        WPP_DEFINE_BIT(MYDRIVER_ALL_INFO)        /* bit  0 = 0x00000001 */ \
-        WPP_DEFINE_BIT(TRACE_DRIVER)             /* bit  1 = 0x00000002 */ \
-        WPP_DEFINE_BIT(TRACE_DEVICE)             /* bit  2 = 0x00000004 */ \
-        WPP_DEFINE_BIT(TRACE_QUEUE)              /* bit  3 = 0x00000008 */ \
+        WPP_DEFINE_BIT(WPP_BIT_ALL_INFO)        /* bit  0 = 0x00000001 */ \
+        WPP_DEFINE_BIT(WPP_BIT_TRACE_DRIVER)             /* bit  1 = 0x00000002 */ \
+        WPP_DEFINE_BIT(WPP_BIT_TRACE_DEVICE)             /* bit  2 = 0x00000004 */ \
+        WPP_DEFINE_BIT(WPP_BIT_TRACE_QUEUE)              /* bit  3 = 0x00000008 */ \
         )
 
 #define WPP_LEVEL_FLAGS_LOGGER(lvl,flags)                              \
@@ -17,16 +17,23 @@
 
 // begin_wpp config
 // USEPREFIX (TraceEntry, "%!STDPREFIX!");
-// FUNC TraceEntry{LEVEL=TRACE_LEVEL_VERBOSE}(...);
+// FUNC TraceEntry{LEVEL=TRACE_LEVEL_VERBOSE, FLAGS=WPP_BIT_ALL_INFO}(...);
 // USESUFFIX (TraceEntry, "--> %!FUNC!");
 // end_wpp
 
+//begin_wpp config
+//USEPREFIX (TRACE_RETURN, "%!STDPREFIX!");
+//FUNC TRACE_RETURN{LEVEL=TRACE_LEVEL_VERBOSE, FLAGS=WPP_BIT_ALL_INFO}(EXP);
+//USESUFFIX (TRACE_RETURN, "<-- %!FUNC!:Function Return=0x%x",EXP);
+//end_wpp
 
-// begin_wpp config
-// USEPREFIX (TraceExit, "%!STDPREFIX!");
-// FUNC TraceExit{LEVEL=TRACE_LEVEL_VERBOSE}(EXP);
-// USESUFFIX (TraceExit, "<-- %!FUNC!: 0x%x", EXP);
-// end_wpp
+// WPP preprocessor will call WPP_Param1_Param2_Param3_ENABLED and WPP_Param1_Param2_Param3_LOGGER in tmh
+// Here, Param1 is LEVEL, Param1 is FLAGS, Param3 is EXP, so we need define them here. 
+// They are for checking if the event should be logged, and for
+// choosing the logger handle to use when calling the ETW trace API.
+// If we have Param4, we need include Param4 in the MACRO also
+#define WPP_LEVEL_FLAGS_EXP_ENABLED(lvl,flags, HR) (WPP_LEVEL_ENABLED(flags) && WPP_CONTROL(WPP_BIT_ ## flags).Level >= lvl)
+#define WPP_LEVEL_FLAGS_EXP_LOGGER(lvl,flags, HR) WPP_LEVEL_LOGGER(flags)
 
 //
 // This comment block is scanned by the trace preprocessor to define the 
